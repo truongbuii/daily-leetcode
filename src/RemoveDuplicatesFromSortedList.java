a4fdeb8bf7a5fd725e748301d95ff2162bed10d2
truongbuii <truongbn.dev@gmail.com>
2024-09-22 20:40:50 +0700

    ADD: leetcode Sep 22, 2024

---
+++ src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,40 @@
+public class RemoveDuplicatesFromSortedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+        ListNode result = deleteDuplicates(head);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+}
