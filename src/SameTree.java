549bc5d52ca8f89a106079823b33c2f4dc83eb82
truongbuii <truongbn.dev@gmail.com>
2024-09-25 21:07:45 +0700

    ADD: Same Tree Sep 25, 2024

---
+++ src/SameTree.java
@@ -0,0 +1,34 @@
+public class SameTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(1), new TreeNode(3));
+        System.out.println(isSameTree(p, q));
+    }
+}
