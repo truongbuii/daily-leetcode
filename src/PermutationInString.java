50d783d7c22d5dfa06dc89ed9dbb4eeafefd7472
truongbuii <truongbn.dev@gmail.com>
2024-10-05 21:09:30 +0700

    ADD: Permutation in String - Oct 5, 2024

---
+++ src/PermutationInString.java
@@ -0,0 +1,39 @@
+public class PermutationInString {
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1Count, s2Count)) {
+                return true;
+            }
+            s2Count[s2.charAt(i + s1.length()) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']--;
+        }
+
+        return matches(s1Count, s2Count);
+    }
+
+    private static boolean matches(int[] s1Count, int[] s2Count) {
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] != s2Count[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1, s2));
+    }
+}
