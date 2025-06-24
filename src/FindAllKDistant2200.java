a0d9cb7c43f35dd5b04fe40c639b2ee2c6cf57ab
truongbuii <truongbn.dev@gmail.com>
2025-06-24 22:55:01 +0700

    Find All K-Distant Indices in an Array Customize One Pass

--- src/FindAllKDistant2200.java
+++ src/FindAllKDistant2200.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class FindAllKDistant2200 {
     // Brute force
-    public static List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+    public static List<Integer> findKDistantIndicesBruteForce(int[] nums, int key, int k) {
         List<Integer> rs = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -17,8 +17,26 @@ public class FindAllKDistant2200 {
         return rs;
     }
 
+    // Brute force
+    public static List<Integer> findKDistantIndicesOnePass(int[] nums, int key, int k) {
+        List<Integer> rs = new ArrayList<>();
+        int r = 0;
+        int n = nums.length;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == key ) {
+                    int l = Math.max(r, j - k);
+                    r = Math.min(n - 1, j + k);
+                    for (int i = l; i <= r; ++i) {
+                        rs.add(i);
+                    }
+                }
+            }
+
+        return rs;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{3,4,9,1,3,9,5};
-        System.out.println(findKDistantIndices(nums, 9, 1));
+        System.out.println(findKDistantIndicesOnePass(nums, 9, 1));
     }
 }
