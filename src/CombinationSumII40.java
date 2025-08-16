3f801a69e3f8bfd3ad88e1acddc411b5888db0c2
truongbuii <truongbn.dev@gmail.com>
2025-08-16 16:23:05 +0700

    Combination Sum II

---
+++ src/CombinationSumII40.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumII40 {
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // 1 1 2 5 6 7 10
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+
+        dfs(candidates, target, 0, new ArrayList<Integer>(), res);
+        return res;
+    }
+
+    private static void dfs(int[] candidates, int target, int start, List<Integer> comb, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i-1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            comb.add(candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1, comb, res);
+            comb.remove(comb.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
+        List<List<Integer>> result = combinationSum2(candidates, target);
+        System.out.println(result);
+    }
+}
